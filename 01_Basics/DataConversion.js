10fc174967a355c023ae96771c0b459f99cb3e34
Shubham Maji <122548421+shubhammaji1@users.noreply.github.com>
2024-02-29 07:00:24 +0000

    DataConversion

---
+++ 01_Basics/DataConversion.js
@@ -0,0 +1,22 @@
+//let score = "56abc";
+//let score = null;
+//let score = undefined;
+//let score = true;
+//console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// let isvalue = ""
+//let isvalue = "hhg"
+// let isvalue = null
+
+// let isboolean = Boolean(isvalue);
+// console.log(isboolean);
+
+let value = 66
+
+let stringval = String(value);
+console.log(stringval);
+console.log(typeof stringval);
