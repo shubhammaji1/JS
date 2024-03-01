d2641f3fbd0d00fb291ffe77bef7c76377e7f41d
Shubham Maji <122548421+shubhammaji1@users.noreply.github.com>
2024-03-01 11:41:40 +0000

    Conversion

---
+++ 01_Basics/StringConversion.js
@@ -0,0 +1,11 @@
+let value =3;
+let negvalue = -value
+//console.log(negvalue);
+
+console.log(1+"2");
+console.log("1"+2);
+console.log("1"+"2");
+console.log(2+1+"2");
+console.log(1+"2"+2);
+console.log(1+"2"+"2");
+console.log(+true);
