868672c0606f8907696c09f04708cd5900b71862
Shubham Maji <122548421+shubhammaji1@users.noreply.github.com>
2024-03-02 13:20:38 +0000

    Strings

---
+++ 01_Basics/Strings.js
@@ -0,0 +1,32 @@
+const name = "shubham"
+const newName = new String('   Shubham-SM   ');
+const repo = 50
+
+// console.log(name + repo +2);
+
+// console.log(newName[0]);
+// console.log(newName.__proto__);
+
+// console.log(newName.length);
+
+
+// console.log(`My name is ${name} and my repo count is ${repo}`);
+
+// const newString = newName.substring(0,5);
+// console.log(newString);
+
+// const anotherString = newName.slice(3,-5);
+// console.log(anotherString);
+
+// console.log(newName);
+// console.log(newName.trim());
+
+const url = "https://www.kme/kimjho%20llo";
+
+const newUrl = url.replace('%20','-');
+console.log(newUrl);
+
+console.log(newName.split('-'));
+
+
+
