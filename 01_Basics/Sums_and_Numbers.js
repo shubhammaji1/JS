57331e78422f3d4eedfc585e7b9fb019ad8752fa
Shubham Maji <122548421+shubhammaji1@users.noreply.github.com>
2024-03-02 14:46:06 +0000

    Math

---
+++ 01_Basics/Sums_and_Numbers.js
@@ -0,0 +1,25 @@
+const balance = new Number(415/2);
+// console.log(balance);
+
+// console.log(balance.toFixed(2));
+
+const otherNumber = 2451.199
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 100000000;
+// console.log(hundreds.toLocaleString('en-In'));
+
+// console.log(Math.abs(-9));
+// console.log(Math.round(3.6));
+// console.log(Math.ceil(3.6));
+// console.log(Math.floor(3.6));
+// console.log(Math.pow(9,2));
+// console.log(Math.min(4,5,8,7));
+
+console.log((Math.random()*10)+1)
+console.log((Math.random()*10)+1)
+
+const Min = 10;
+const Max = 20;
+
+console.log(Math.floor(Math.random()*(Max-Min+1))+Min);
