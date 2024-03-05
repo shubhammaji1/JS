f520548f0c915b836535c18976e55a2e2100f5b2
Shubham Maji <122548421+shubhammaji1@users.noreply.github.com>
2024-03-05 20:25:02 +0000

    Scope

---
+++ 03_Basics/Scope.js
@@ -0,0 +1,33 @@
+if(true){
+    let a =10
+    const b =20
+    var c = 30
+}
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+
+function one(){
+    const username ="Shubham"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+one()
+
+//++++++++++++++++ Interesting+++++++++++
+console.log(addon(5))
+function addon(num){
+    return num+1;
+}
+
+console.log(add(5))
+const add = function addvalue(num){
+        return num+2;
+        }
+
