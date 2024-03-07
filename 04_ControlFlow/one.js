94e579c45ecfce76936a85b9bc62bb267949c3f5
Shubham Maji <122548421+shubhammaji1@users.noreply.github.com>
2024-03-07 09:55:45 +0000

    ControlFlow

---
+++ 04_ControlFlow/one.js
@@ -0,0 +1,20 @@
+// let score =200
+
+// if(score>100){
+//     let power = "fly"
+//     console.log(`user Power: ${power}`)
+// }
+// console.log(power)
+const balance = 1000
+// if(balance>500) console.log("balance"),console.log("Test");
+
+if(balance<500){
+    console.log("less than")
+}
+else if(balance<750){
+    console.log("not");
+}
+else{
+    console.log("less than 2000");
+}
+
