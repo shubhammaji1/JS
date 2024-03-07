07bdf6109dbadc8f8f26ce3138d184109fdd770d
Shubham Maji <122548421+shubhammaji1@users.noreply.github.com>
2024-03-07 18:02:50 +0000

    for

---
+++ Iterations.js/one.js
@@ -0,0 +1,19 @@
+for (let i = 1; i <= 10; i++) {
+    for (let j = 1; j <= 10; j++) {
+        // console.log(`${i}*${j}=${i*j}`)
+        
+    } 
+}
+let myArr =['Flash','Ironman','Superman']
+for (let i = 0; i < myArr.length; i++) {
+    const element = myArr[i];
+    // console.log(element);    
+}
+for (let i = 1; i < 10; i++) {
+    if(i == 5){
+        console.log("Detected 5");
+        break
+    }
+    console.log(`value of i is${i}`)
+    
+}
