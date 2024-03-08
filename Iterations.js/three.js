437108740e7fbe869d737e69cfe2957853c66ca1
Shubham Maji <122548421+shubhammaji1@users.noreply.github.com>
2024-03-08 10:29:14 +0000

    highOrder

---
+++ Iterations.js/three.js
@@ -0,0 +1,28 @@
+//for of
+
+const arr = [1,2,3,6,5,4]
+for (const val of arr) {
+    // console.log(val);
+}
+
+// Map
+const map = new Map()
+map.set('IN',"INDIA")
+map.set('USA',"UNITED STATE OF AMERICA")
+map.set('FR',"FRANCE")
+
+// console.log(map);
+for (const [key,value] of map) {
+    // console.log(key,":-",value);
+}
+
+const myObj ={
+    'name':"Shubham",
+    'roll':121,
+    'Email' : "abc@gmail.com"
+}
+
+for (const [key,value] of myObj) {
+    console.log(i)
+}
+
